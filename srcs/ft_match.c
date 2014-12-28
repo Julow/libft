d5c219ea5f7ed3ddc9b66e7ae71608327dbff920
juloo <juloo.dsi@gmail.com>
2014-12-28 15:05:30 +0100

    Optimise match and nmatch

--- srcs/ft_match.c
+++ srcs/ft_match.c
@@ -14,11 +14,18 @@
 
 t_bool			ft_match(char *str, char *pattern)
 {
-	if (*str == '\0' && *pattern == '*')
-		return (ft_match(str, pattern + 1));
+	while (*str == '\0' && *pattern == '*')
+		pattern++;
 	if (*str == '\0' || *pattern == '\0')
-		return ((*str == '\0' && *pattern == '\0') ? 1 : 0);
-	if (*pattern == '*')
-		return (ft_match(str, pattern + 1) | ft_match(str + 1, pattern));
-	return ((*str == *pattern) ? ft_match(str + 1, pattern + 1) : 0);
+		return ((*str == *pattern) ? TRUE : FALSE);
+	if (*pattern == '*' && ft_match(str + 1, pattern++))
+		return (TRUE);
+	if (*str != *pattern)
+		return (FALSE);
+	while (*str == *pattern && *str != '\0' && *pattern != '*')
+	{
+		str++;
+		pattern++;
+	}
+	return (ft_match(str, pattern));
 }
--- srcs/ft_nmatch.c
+++ srcs/ft_nmatch.c
@@ -14,10 +14,10 @@
 
 int				ft_nmatch(char *str, char *pattern)
 {
-	if (*str == '\0' && *pattern == '*')
-		return (ft_nmatch(str, pattern + 1));
+	while (*str == '\0' && *pattern == '*')
+		pattern++;
 	if (*str == '\0' || *pattern == '\0')
-		return ((*str == '\0' && *pattern == '\0') ? 1 : 0);
+		return ((*str == *pattern) ? 1 : 0);
 	if (*pattern == '*')
 		return (ft_nmatch(str, pattern + 1) + ft_nmatch(str + 1, pattern));
 	return ((*str == *pattern) ? ft_nmatch(str + 1, pattern + 1) : 0);
