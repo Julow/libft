88fa7cb9e78adb15ffeb92f75161773032d44aae
jaguillo <juloo.dsi@gmail.com>
2014-12-29 19:58:05 +0100

    ft_lstdelnext

--- libft.h
+++ libft.h
@@ -283,6 +283,7 @@ void			ft_putnbr_fd(int n, int fd);
 */
 t_list			*ft_lstnew(void const *content, size_t content_size);
 void			ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
+void			ft_lstdelnext(t_list *lst, void (*f)(void*, size_t));
 void			ft_lstdel(t_list **alst, void (*del)(void*, size_t));
 void			ft_lstadd(t_list **alst, t_list *new);
 void			ft_lstafter(t_list *lst, t_list *add);
---
+++ srcs/ft_lstdelnext.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelnext.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/12/29 19:52:35 by jaguillo          #+#    #+#             */
+/*   Updated: 2014/12/29 19:52:35 by jaguillo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void			ft_lstdelnext(t_list *lst, void (*f)(void*, size_t))
+{
+	t_list			*tmp;
+
+	if (lst == NULL || lst->next == NULL)
+		return ;
+	tmp = lst->next;
+	lst->next = tmp->next;
+	if (f != NULL)
+		f(tmp->content, tmp->content_size);
+	free(tmp);
+}
