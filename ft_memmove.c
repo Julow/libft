8635ed415ec9a07f51834b06a4f75f9b6ff35095
jaguillo <juloo.dsi@gmail.com>
2014-12-30 11:47:47 +0100

    Fix memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -19,20 +19,20 @@ void			*ft_memmove(void *dst, const void *src, size_t len)
 
 	if (src >= dst)
 		return (ft_memcpy(dst, src, len));
-	tmp_dst = dst + len - 1;
-	tmp_src = ((void*)src) + len - 1;
+	tmp_dst = dst + len;
+	tmp_src = ((void*)src) + len;
 	while (len > 7)
 	{
-		*((t_ulong*)tmp_dst) = *((t_ulong*)tmp_src);
 		tmp_dst = VOIDADD(tmp_dst, -8);
 		tmp_src = VOIDADD(tmp_src, -8);
+		*((t_ulong*)tmp_dst) = *((t_ulong*)tmp_src);
 		len -= 8;
 	}
 	while (len > 0)
 	{
-		*((t_byte*)tmp_dst) = *((t_byte*)tmp_src);
 		VOIDMM(tmp_dst);
 		VOIDMM(tmp_src);
+		*((t_byte*)tmp_dst) = *((t_byte*)tmp_src);
 		len--;
 	}
 	return (dst);
