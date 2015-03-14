b3da2b2746d5c40e4ee002cf183bab3a6df738df
juloo <juloo.dsi@gmail.com>
2015-03-15 00:47:33 +0100

    Fix and optimise ft_writenchar

--- srcs/ft_buff/write/ft_writenchar.c
+++ srcs/ft_buff/write/ft_writenchar.c
@@ -6,7 +6,7 @@
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/11 21:21:01 by jaguillo          #+#    #+#             */
-/*   Updated: 2015/02/11 21:21:32 by jaguillo         ###   ########.fr       */
+/*   Updated: 2015/03/15 00:46:34 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 
 void			ft_writenchar(t_buff *buff, char c, int n)
 {
-	while (n--)
-		ft_writechar(buff, c);
+	int				free_buff;
+
+	while (true)
+	{
+		free_buff = buff->length - buff->i;
+		if (n <= free_buff)
+			break ;
+		ft_writenchar(buff, c, free_buff);
+		ft_flush(buff);
+		n -= free_buff;
+	}
+	ft_memset(buff->data + buff->i, c, n);
+	buff->i += n;
 }
