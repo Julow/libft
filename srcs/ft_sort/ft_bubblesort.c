00ae36ffdb6c99b693cecb0d82630db271fdb872
juloo <juloo.dsi@gmail.com>
2015-04-27 23:38:43 +0200

    Nothing

--- srcs/ft_sort/ft_bubblesort.c
+++ srcs/ft_sort/ft_bubblesort.c
@@ -6,7 +6,7 @@
 /*   By: juloo <juloo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/04/27 19:28:14 by juloo             #+#    #+#             */
-/*   Updated: 2015/04/27 19:36:14 by juloo            ###   ########.fr       */
+/*   Updated: 2015/04/27 22:30:48 by juloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void			ft_bubblesort(void **tab, int length, int (*cmp)())
 	i = 1;
 	while (i < length)
 	{
-		if (cmp(tab[i - 1], tab[i]) > 0)
+		if (i >= 1 && cmp(tab[i - 1], tab[i]) > 0)
 		{
 			tmp = tab[i];
 			tab[i] = tab[i - 1];
--- tests/sort/test_sort.c
+++ tests/sort/test_sort.c
@@ -6,7 +6,7 @@
 /*   By: juloo <juloo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/04/27 15:03:15 by juloo             #+#    #+#             */
-/*   Updated: 2015/04/27 19:43:30 by juloo            ###   ########.fr       */
+/*   Updated: 2015/04/27 22:38:13 by juloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,18 @@
 
 int				g_cmp_count = 0;
 
+char const		*g_best_name = NULL;
+t_ulong			g_best_time = 0;
+int				g_best_cmp = 0;
+
 static int		test_cmp(char *s1, char *s2)
 {
 	g_cmp_count++;
 	return (ft_strcmp(s1, s2));
 }
 
-static void		test(char **argv, int length, void (*sort)(void **, int, int (*)()), char const *name)
+static void		test(char **argv, int length,
+	void (*sort)(void **, int, int (*)()), char const *name)
 {
 	char			*array[length];
 	int				i;
@@ -31,10 +36,15 @@ static void		test(char **argv, int length, void (*sort)(void **, int, int (*)())
 	ft_memcpy(array, argv, S(char*, length));
 	g_cmp_count = 0;
 	start = ft_clock(0);
-	ft_printf("%{red}In progress%{reset}");
 	sort((void**)array, length, &test_cmp);
 	start = ft_clock(start);
-	ft_printf("\r%{cyan}%s%{reset}%>27 cmp: %-10d time: %'lluns\n", name, g_cmp_count, start);
+	if (g_best_name == NULL || g_cmp_count < g_best_cmp)
+	{
+		g_best_name = name;
+		g_best_time = start;
+		g_best_cmp = g_cmp_count;
+	}
+	ft_printf("(%d %lluns) ", g_cmp_count, start);
 	i = -1;
 	if (!ft_issort((void**)array, length, &test_cmp))
 	{
@@ -47,9 +57,10 @@ static void		test(char **argv, int length, void (*sort)(void **, int, int (*)())
 
 int				main(int argc, char **argv)
 {
-	ft_printf("[%{green}len: %d%{reset}]\n", argc - 1);
 	TEST(ft_simplesort);
 	TEST(ft_mergesort);
 	TEST(ft_bubblesort);
+	ft_printf("%{green}%s %{cyan}%dns (cmp: %d, len: %d)%{reset}\n",
+		g_best_name, g_best_time, g_best_cmp, argc - 1);
 	return (0);
 }
