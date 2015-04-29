71997a151a3e962d0202ced69c8453e4dd2c230b
juloo <juloo.dsi@gmail.com>
2015-04-30 00:16:13 +0200

    Fix quicksort

--- srcs/ft_sort/ft_quicksort.c
+++ srcs/ft_sort/ft_quicksort.c
@@ -6,7 +6,7 @@
 /*   By: juloo <juloo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/04/28 17:18:35 by juloo             #+#    #+#             */
-/*   Updated: 2015/04/29 18:20:18 by juloo            ###   ########.fr       */
+/*   Updated: 2015/04/29 23:16:03 by juloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,17 @@ void			ft_quicksort(void **tab, int length, int (*cmp)())
 	int				left;
 	void			*tmp;
 
-	if (length <= 4)
-		return (ft_insertsort(tab, length, cmp));
+	if (length <= 3)
+		return (ft_bubblesort(tab, length, cmp));
 	pivot = length / 2;
 	right = pivot;
 	left = pivot;
 	while (true)
 	{
-		while (right < length)
-		{
-			if (cmp(tab[right], tab[pivot]) < 0)
-				break ;
+		while (right < length && cmp(tab[right], tab[pivot]) >= 0)
 			right++;
-		}
-		while (left >= 0)
-		{
-			if (cmp(tab[left], tab[pivot]) > 0)
-				break ;
+		while (left >= 0 && cmp(tab[left], tab[pivot]) <= 0)
 			left--;
-		}
 		if (left < 0)
 		{
 			if (right >= length)
