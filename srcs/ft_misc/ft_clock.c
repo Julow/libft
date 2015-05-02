fe13824afd7ef54ca5832e90e541f654d5062fce
jaguillo <juloo.dsi@gmail.com>
2015-05-02 13:03:29 +0200

    Fix ft_clock on MacOS

--- srcs/ft_misc/ft_clock.c
+++ srcs/ft_misc/ft_clock.c
@@ -6,17 +6,24 @@
 /*   By: juloo <juloo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/04/27 19:23:34 by juloo             #+#    #+#             */
-/*   Updated: 2015/04/27 19:24:04 by juloo            ###   ########.fr       */
+/*   Updated: 2015/05/02 13:03:52 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 #include <time.h>
+
+#ifdef __MACH__
+# define GET_CLOCK(tmp)		tmp = (struct timespec){0, clock() * 1000}
+#else
+# define GET_CLOCK(tmp)		clock_gettime(CLOCK_MONOTONIC, &tmp)
+#endif
 
 t_ulong			ft_clock(t_ulong start)
 {
 	struct timespec	tmp;
 
-	clock_gettime(CLOCK_MONOTONIC, &tmp);
+	GET_CLOCK(tmp);
 	return (tmp.tv_sec * FT_CPS + tmp.tv_nsec - start);
 }
