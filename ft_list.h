77e412f8de66593c697118695c0cfb00385852d0
juloo <juloo.dsi@gmail.com>
2015-08-16 02:27:14 +0200

    Better prototypes

--- ft_list.h
+++ ft_list.h
@@ -6,7 +6,7 @@
 /*   By: juloo <juloo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/16 01:00:32 by juloo             #+#    #+#             */
-/*   Updated: 2015/08/16 01:19:47 by juloo            ###   ########.fr       */
+/*   Updated: 2015/08/16 01:58:04 by juloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,8 @@
 
 /*
 ** ========================================================================== **
-** List
+** Double chained list
 ** -
-** Storage
 */
 
 typedef struct	s_l_node
@@ -36,24 +35,30 @@ typedef struct	s_list
 	int const		node_size;
 }				t_list;
 
+/*
+** Create a list of t
+*/
 #define LIST(t)			((t_list){NULL, NULL, 0, sizeof(t)})
 
+/*
+** Create an iterator
+** (the pointer is invalid until LIST_NEXT or LIST_PREV is called)
+*/
 #define LIST_IT(l)		(((void*)(l)) + sizeof(t_l_node))
 
+/*
+** Increment or decrement the iterator
+*/
 #define LIST_NEXT(n)	(*(((void**)(n)) - 2))
 #define LIST_PREV(n)	(*(((void**)(n)) - 1))
 
 /*
-** Create a new node and place it before 'next'
+** Create a new node and add it before 'next'
+** if 'next' is NULL add to the end
+** the node is bzero
 ** Return the new node
 */
-void			*ft_listbefore(t_list *lst, void *next);
-
-/*
-** Create a new node and place it after 'prev'
-** Return the new node
-*/
-void			*ft_listafter(t_list *lst, void *prev);
+void			*ft_listadd(t_list *lst, void *next);
 
 /*
 ** Remove a node
