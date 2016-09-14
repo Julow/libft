f8bf3123bdff2e2ac925cec366150f0efdafc1c8
juloo <juloo>
2016-09-14 22:47:12 +0200

    Move buff_in to it's module

---
+++ ft_buff_in/public/buff_in.h
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   buff_in.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juloo <juloo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/14 22:18:02 by juloo             #+#    #+#             */
+/*   Updated: 2016/09/14 22:19:12 by juloo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUFF_IN_H
+# define BUFF_IN_H
+
+# include "ft/ft_in.h"
+# include "ft/libft.h"
+
+typedef struct s_buff_in			t_buff_in;
+
+/*
+** ========================================================================== **
+** Buffered in
+** -
+** TODO: export to it's own module
+*/
+
+/*
+** in				=> source stream
+** buff				=> buffer
+** buff_end			=> end offset in the buffer
+** buff_capacity	=> allocated buff size
+*/
+struct			s_buff_in
+{
+	t_in			*in;
+	char			*buff;
+	uint32_t		i;
+	uint32_t		length;
+	uint32_t		buff_capacity;
+};
+
+# define BUFF_IN(IN)		((t_buff_in){(IN), NULL, 0, 0, 0})
+
+# define BUFF_IN_MIN_BUFF	32
+
+/*
+** Check if 'B.i + I' is a valid index
+** Read if needed
+*/
+# define BUFF_IN_CHECK(B,I)	(((B).i + (I)) < (B).length || ft_buff_in_read(&(B)))
+
+/*
+** Return char at 'B.i + I'
+*/
+# define BUFF_IN_GETC(B,I)	((B).buff[(B).i + (I)])
+
+/*
+** Read a full buffer from the source stream
+** Discard characters not in the range ['i', 'length')
+** Return true on success, false on EOF
+*/
+bool			ft_buff_in_read(t_buff_in *in);
+
+/*
+** Clear/free
+** The object is reusable after a clear
+*/
+void			ft_buff_in_clear(t_buff_in *in);
+
+#endif
--- ft_tokenizer/public/tokenizer.h
+++ ft_tokenizer/public/tokenizer.h
@@ -6,14 +6,14 @@
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/15 17:19:33 by jaguillo          #+#    #+#             */
-/*   Updated: 2016/09/14 19:52:32 by jaguillo         ###   ########.fr       */
+/*   Updated: 2016/09/14 22:34:40 by juloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOKENIZER_H
 # define TOKENIZER_H
 
-# include "ft/ft_dstr.h"
+# include "ft/buff_in.h"
 # include "ft/ft_in.h"
 # include "ft/libft.h"
 
@@ -23,58 +23,6 @@ typedef struct s_tokenmap_t			t_tokenmap_t;
 typedef struct s_tokenmap			t_tokenmap;
 
 typedef struct s_tokenizer			t_tokenizer;
-
-typedef struct s_buff_in			t_buff_in;
-
-/*
-** ========================================================================== **
-** Buffered in
-** -
-** TODO: export to it's own module
-*/
-
-/*
-** in				=> source stream
-** buff				=> buffer
-** buff_end			=> end offset in the buffer
-** buff_capacity	=> allocated buff size
-*/
-struct			s_buff_in
-{
-	t_in			*in;
-	char			*buff;
-	uint32_t		i;
-	uint32_t		length;
-	uint32_t		buff_capacity;
-};
-
-# define BUFF_IN(IN)		((t_buff_in){(IN), NULL, 0, 0, 0})
-
-# define BUFF_IN_MIN_BUFF	32
-
-/*
-** Check if 'B.i + I' is a valid index
-** Read if needed
-*/
-# define BUFF_IN_CHECK(B,I)	(((B).i + (I)) < (B).length || ft_buff_in_read(&(B)))
-
-/*
-** Return char at 'B.i + I'
-*/
-# define BUFF_IN_GETC(B,I)	((B).buff[(B).i + (I)])
-
-/*
-** Read a full buffer from the source stream
-** Discard characters not in the range ['i', 'length')
-** Return true on success, false on EOF
-*/
-bool			ft_buff_in_read(t_buff_in *in);
-
-/*
-** Clear/free
-** The object is reusable after a clear
-*/
-void			ft_buff_in_clear(t_buff_in *in);
 
 /*
 ** ========================================================================== **
